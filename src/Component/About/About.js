c16daa0dd0b52f83b7fe3cb6413e1c6c890d7e11
mdAshik922 <md.ashik0005723@gmail.com>
2021-12-13 19:53:23 +0600

    commit

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -56,49 +56,8 @@ const About = () => {
         </div>
 
 
-        <div className="skill-bars">
-    <div className="bar">
-      <div className="info">
-        <span>HTML</span>
-      </div>
-      <div className="progress-line html">
-        <span></span>
-      </div>
-    </div>
-    <div className="bar">
-      <div className="info">
-        <span>CSS</span>
-      </div>
-      <div className="progress-line css">
-        <span></span>
-      </div>
-    </div>
-    <div className="bar">
-      <div className="info">
-        <span>jQuery</span>
-      </div>
-      <div className="progress-line jquery">
-        <span></span>
-      </div>
-    </div>
-    <div className="bar">
-      <div className="info">
-        <span>Python</span>
-      </div>
-      <div className="progress-line python">
-        <span></span>
-      </div>
-    </div>
-    <div className="bar">
-      <div className="info">
-        <span>MySQL</span>
-      </div>
-      <div className="progress-line mysql">
-        <span></span>
-      </div>
-    </div>
-  </div>
 
+        
         </div>
     );
 };
