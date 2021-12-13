b61b1a263a00953d35bbf0e8622356f4543896d2
mdAshik922 <md.ashik0005723@gmail.com>
2021-12-13 21:16:54 +0600

    commit

--- src/Component/Service/Service.js
+++ src/Component/Service/Service.js
@@ -3,6 +3,7 @@ import './Service.css';
 import responsive from './img/responsive.png';
 import code from './img/coding.png';
 import debug from './img/debug.png';
+import design from './img/website-design.png';
 
 const Service = () => {
     return (
@@ -10,7 +11,7 @@ const Service = () => {
             <div className="container mt-5">
                <div class="row">
                <div class="col-sm-4" style={{color: "white"}}>
-               <i width="55px" className="fas fa-code text-custom"></i>
+               <img width="55px" style={{backgroundColor: "white"}} src={code} alt="code"/>
                     <h2>Web Design</h2>
                     <p style={{color: "white"}}>
                     There are many variations of passages 
@@ -19,15 +20,6 @@ const Service = () => {
                     </p>
                 </div>
                
-                <div class="col-sm-4" style={{color: "white"}}>
-                
-                    <h2>Graphic Design</h2>
-                    <p>
-                    I can design logo, banner, thumbnail
-                     for your project. I am very experienced
-                      and expert in this fiend.
-                    </p>
-                </div>
                 <div class="col-sm-4" style={{color: "white"}}>
               <img width="55px" src={responsive} alt="responsive"/>
                     <h2>Responsive Design</h2>
@@ -38,7 +30,7 @@ const Service = () => {
                     </p>
                 </div>
                 <div class="col-sm-4" style={{color: "white"}}>
-                <i class="fas fa-tools text-custom"></i>
+                <img width="55px" style={{backgroundColor: "white"}} src={debug} alt="debug"/>
                     <h2>Bugs fixing</h2>
                     <p>
                     I am expert in fixing bugs.
