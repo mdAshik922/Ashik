a13c3b17a0056eb902a574df15d677bcead97b6e
mdAshik922 <md.ashik0005723@gmail.com>
2021-12-13 22:17:07 +0600

    commit

--- src/Component/Service/Service.js
+++ src/Component/Service/Service.js
@@ -27,11 +27,10 @@ const Service = () => {
                     <h2>Responsive Design</h2>
                     <p>
                     I can design responsive webpages.
-                     It will be W3C validated, fresh,
-                      SEO optimized and hand coded.
+                    It will be SEO optimized and hand-coded.
                     </p>
                 </div>
-                
+
                 <div className="col-sm-4" style={{color: "white"}}>
                 <img width="55px" style={{backgroundColor: "white"}} src={debug} alt="debug"/>
                   
