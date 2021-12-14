e7f91cbdbbd507929035003c38abb511533e0341
mdAshik922 <md.ashik0005723@gmail.com>
2021-12-14 23:02:48 +0600

    commit

--- src/Component/Blogs/Blogs.js
+++ src/Component/Blogs/Blogs.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div id="blogs">
-            <h2  style={{color: "white", marginLeft: "30%", marginBottom: "4%", fontSize: "4rem", fontWeight: "bolder"}}>blogs</h2>
+            {/* <h2  style={{color: "white", marginLeft: "30%", marginBottom: "4%", fontSize: "4rem", fontWeight: "bolder"}}>blogs</h2> */}
         </div>
     );
 };
