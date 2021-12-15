788ac8f5d48f8d2448d78658583182a8f5ad94a0
mdAshik922 <md.ashik0005723@gmail.com>
2021-12-15 23:35:48 +0600

    commit

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -5,7 +5,7 @@ import './contact.css';
 const Contact = () => {
     return (
         <div id="contact" style={{marginTop: "5%"}} >
-            <h2 style={{color: "white", marginLeft: "30%", marginBottom: "4%", fontSize: "4rem", fontWeight: "bolder"}}>Contact now</h2>
+            <h2 style={{color: "white", textAling: "center", marginBottom: "4%", fontSize: "4rem", fontWeight: "bolder"}}>Contact now</h2>
        
        <div id="contact-form" style={{}} >
        <form action="https://formsubmit.co/md.ashik0005723@gmail.com" method="POST">
