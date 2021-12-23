93231bccb38f797e01e3ef77c3ddc893cd22c80a
mdAshik922 <md.ashik0005723@gmail.com>
2021-12-23 11:01:39 +0600

    commit

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -23,7 +23,9 @@ const Contact = () => {
 
        </div>
      
-     
+       <div>
+     <p style={{color: "white", textAlign: 'center', marginTop: "3%"}}> &copy;2021 <span style={{fontWeight: "bolder"}}>Ashik</span>. All Rights Reserved</p>
+      </div>
      
         </div>
     );
