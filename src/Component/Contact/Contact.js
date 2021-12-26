595d959f9ee4ad65a859f85c0920a3a293d968f8
mdAshik922 <md.ashik0005723@gmail.com>
2021-12-26 07:43:51 +0600

    commit

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -15,10 +15,10 @@ const Contact = () => {
      <input style={{width: "65%", marginLeft: "10%", paddingLeft: "1%" }} type="email" name="email" placeholder="Email Address" required />
      <br/>
      <br/>
-     <textarea style={{width: "75%", marginLeft: "10%", paddingLeft: "1%" }} name="message" id="" cols="30" rows="10" placeholder="Message"></textarea >
+     <textarea style={{ width: "75%", marginLeft: "10%", paddingLeft: "1%" }} name="message" id="" cols="30" rows="10" placeholder="Message"></textarea >
      <br/>
      <input type="hidden" name="_captcha" value="false" />
-     <button style={{color: "blue", width: "35%", marginTop: "10px", marginLeft: "20%", marginBottom: "5%" , backgroundColor: "red", fontSize: "2rem", fontWeight: "bold", borderRadius: '10px'}} type="submit">Send<i className="far fa-paper-plane"></i></button>
+     <button style={{ color: "blue", width: "35%", marginTop: "10px", marginLeft: "20%", marginBottom: "5%" , backgroundColor: "red", fontSize: "2rem", fontWeight: "bold", borderRadius: '10px'}} type="submit">Send<i className="far fa-paper-plane"></i></button>
 </form>
 
        </div>
