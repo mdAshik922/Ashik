17d8ac2ee9748c82afc0c4d4d56ce7635659a3a8
mdAshik922 <md.ashik0005723@gmail.com>
2021-12-28 10:32:06 +0600

    commit

--- src/Component/Blogs/Blogs.js
+++ src/Component/Blogs/Blogs.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
 const Blogs = () => {
+
+    const medium = {
+        href:"https://medium.com/@md.ashik0005723/explained-for-beginners-about-javascript-js-c5c80f7c3aa6",
+         target:"_blank"
+      };
+
     return (
         <div id="blog">
             <h2 style={{color: "white", textAlign: "center", marginTop: "4%", fontSize: "4rem", fontWeight: "bolder"}}>My Blog</h2>
 
 <div>
-
+<a style={{textDecoration: "none"}} {...medium} rel="noreferrer">
+<img width="50%" src="https://i.ibb.co/qLSM1Fs/download.png" alt="blog"/>
+<h3 style={{color: "white"}}>Javascript for Beginner</h3>
+<p style={{color: "blue"}}>...Learn more</p>
+</a>
 </div>
         </div>
     );
