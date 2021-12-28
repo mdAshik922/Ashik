d6fd80cab79d80033f53604eb8c8f3a3a81434bf
mdAshik922 <md.ashik0005723@gmail.com>
2021-12-28 10:58:40 +0600

    commit

--- src/Component/Blogs/Blogs.js
+++ src/Component/Blogs/Blogs.js
@@ -9,7 +9,8 @@ const Blogs = () => {
 
     return (
         <div id="blog">
-            <h2 style={{color: "white", textAlign: "center", marginTop: "4%", fontSize: "4rem", fontWeight: "bolder"}}>My Blog</h2>
+            <h2 style={{color: "white", textAlign: "center", marginTop: "4%", fontSize: "4rem", fontWeight: "bolder"}}>
+            <i className="far fa-edit"></i>My Blog</h2>
 
 <div style={{marginLeft: "35%", marginTop: "3%"}}>
 <a style={{textDecoration: "none"}} {...medium} rel="noreferrer">
