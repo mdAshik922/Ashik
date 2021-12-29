acb6f1f16c76f937962c7c39718c27cc17ddb271
mdAshik922 <md.ashik0005723@gmail.com>
2021-12-29 21:02:40 +0600

    commit

--- src/Component/Service/Service.js
+++ src/Component/Service/Service.js
@@ -13,30 +13,26 @@ const Service = () => {
                <div className="col-sm-4" style={{color: "white"}}>
 
                <img className="service-img" width="55px" src={code} alt="code"/>
-                    <h2 className="service-head-line">Web Design</h2>
+                  <h2 className="service-head-line">Web Design</h2>
                     <p>
                     There are many variations of passages 
                     of but the majority have suffered alteration
                      in some form.
                     </p>
-                    
-                </div>
+                           </div>
                
                 <div className="col-sm-4" style={{color: "white"}}>                 
               <img className="service-img" width="55px" src={responsive} alt="responsive"/>
-
-                    <h2 className="service-head-line">Responsive Design</h2>
+                  <h2 className="service-head-line">Responsive Design</h2>
                     <p>
                     I can design responsive webpages.
                     It will be SEO optimized and hand-coded.
                     </p>
-                  
-                </div>
+                       </div>
 
                 <div className="col-sm-4" style={{color: "white"}}>
                 <img className="service-img" width="55px" src={debug} alt="debug"/>
-                  
-                    <h2 className="service-head-line">Bugs fixing</h2>
+                   <h2 className="service-head-line">Bugs fixing</h2>
                     <p>
                     I am expert in fixing bugs.
                      if your website have also problem 
