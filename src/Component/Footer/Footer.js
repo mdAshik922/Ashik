f908a7d37c1f080d0dc25cd38e8cbecbadba6da5
mdAshik922 <md.ashik0005723@gmail.com>
2021-12-29 19:12:40 +0600

    commit

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            
+             <div>
+     <p style={{ color: "white", textAlign: 'center', marginTop: "3%" }}>
+        &copy;2021 <span style={{fontWeight: "bolder"}}>Ashik</span>. All Rights Reserved</p>
+      </div>
         </div>
     );
 };
