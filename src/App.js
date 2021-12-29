b4b50b1cb306ce0f2b86aa858c509dc01a50b06e
mdAshik922 <md.ashik0005723@gmail.com>
2021-12-29 20:05:09 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,15 @@ import Header from './Component/Header/Header';
 import Home from './Component/Home/Home';
 
 
-
-
-
-
 function App() {
+
   return (
     <div  className="App">
   <BrowserRouter>
  <Header></Header>
   <Routes>
-   <Route  path="/" element={<Home></Home>} />
-   <Route path="/home" element={<Home></Home>} />
+   <Route  path="/" element={<Home/>} />
+   <Route path="/home" element={<Home/>} />
   </Routes>
   <Footer></Footer>
   </BrowserRouter>
